db65c215b1d4adf96471d56fee794527e772860b
Al Chyrill Cabrera <Chyrill.Cabrera@kwp.com.au>
2020-05-11 05:50:39 +0800

    added compare sort

--- index.js
+++ index.js
@@ -15,9 +15,24 @@ class ErrorResponse {
 }
 
 
+const compareSort = (property) => {
+    return (a, b) => {
+        const itemA =  a[property]
+        const itemB = b[property]
+    
+        let comparison = 0
+        if(itemA > itemB)
+            comparison = 1
+        else if (itemB > itemA)
+            comparison = -1
+        return comparison
+    }
+}
+
 
 module.exports = {
     ErrorResponse,
     ServiceHost,
-    RequestValidationMiddleware
+    RequestValidationMiddleware,
+    compareSort
 }
