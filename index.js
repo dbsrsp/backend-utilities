f111e8e818811395bdebef6405a5783aba71d15d
dragonjet <jetriconew@gmail.com>
2020-06-18 15:34:52 -0700

    backend utilities update

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-const RequestValidationMiddleware = require('./RequestValidationMiddleware')
-const ServiceHost = require('./ServiceHost')
+const RequestValidationMiddleware = require('./middleware/RequestValidationMiddleware');
+const ServiceHost = require('./service/ServiceHost');
+
 class ErrorResponse {
-    code = ''
-    message = ''
+  constructor(code, message) {
+    this.code = code;
+    this.message = message;
+  }
 
-    constructor(code, message) {
-        this.code = code
-        this.message = message
-    }
-
-    toString() {
-        return JSON.stringify({ code: this.code, message: this.message })
-    }
+  toString() {
+    return JSON.stringify({ code: this.code, message: this.message });
+  }
 }
 
+const compareSort = (property) => ((a, b) => {
+  const itemA = a[property];
+  const itemB = b[property];
 
-const compareSort = (property) => {
-    return (a, b) => {
-        const itemA =  a[property]
-        const itemB = b[property]
-    
-        let comparison = 0
-        if(itemA > itemB)
-            comparison = 1
-        else if (itemB > itemA)
-            comparison = -1
-        return comparison
-    }
-}
-
+  let comparison = 0;
+  if (itemA > itemB) comparison = 1;
+  else if (itemB > itemA) comparison = -1;
+  return comparison;
+});
 
 module.exports = {
-    ErrorResponse,
-    ServiceHost,
-    RequestValidationMiddleware,
-    compareSort
-}
+  ErrorResponse,
+  ServiceHost,
+  RequestValidationMiddleware,
+  compareSort,
+};
