6fbdc03de3edd9df63b31756ae5d0d406cde834d
Al Chyrill Cabrera <chyrill.cabrera@gmail.com>
2020-09-07 10:58:13 +0800

    check error on get quotation details

--- QuotationService.js
+++ QuotationService.js
@@ -4,6 +4,7 @@ const GetQuotationDetails = async(quotationId) => {
 
         return response.data
     } catch(err) {
+        console.log(err)
         throw new Error(err.error.message)
     }
 }
