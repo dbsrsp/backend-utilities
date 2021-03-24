cbabbcae7e7bf99f2613c8aec499f8c82f255aa5
Al Chyrill Cabrera <chyrill.cabrera@gmail.com>
2021-03-24 12:34:31 +0800

    update on geofenceService.GetGeofencePricing

--- GeofenceService.js
+++ GeofenceService.js
@@ -1,12 +1,13 @@
 const ServiceHost = require('./ServiceHost')
 const axios = require('axios').default
 
-const GetGeofencePricing = async(pickupLocation, dropoffLocation, brandId) => {
+const GetGeofencePricing = async(pickupLocation, dropoffLocation, brandId, serviceAreaId = null) => {
     try {
         const response = await axios.post(ServiceHost.get('geofence') + '/service/geofence.get.pricing', {
             pickUpLocation: pickupLocation,
             dropOffLocation: dropoffLocation,
-            brandId
+            brandId,
+            serviceAreaId
         })
 
         return response.data
